de975d5d709303cb84704fbf5d294614e715843d
kingnan <373734199@qq.com>
2024-06-07 21:31:17 +0800

    反射

---
+++ testOne/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+namespace testNameSpace
+{
+
+
+
+    public class testClass
+    {
+        public int a = 11;
+        public string b = "Hello, World!";
+
+        public enum Color
+        {
+            red,
+            black,
+        };
+
+
+        public testClass(int i, string j,  Color data)
+        {
+            // Console.WriteLine("Hello, World!");
+            this.a = i;
+            this.b = j;
+
+            if(data == Color.red){
+                // Console.WriteLine(" =  Color.red");
+                this.a = 223;
+            }
+
+        }
+
+        public void testMethod()
+        {
+            Console.WriteLine("testMethod a = " + a +" b =" + b);
+        }
+        public static void Main()
+        {
+            testClass tc = new testClass(12, "Hello, World!", Color.red);
+            Console.WriteLine(tc.a);
+            Console.WriteLine(tc.b);
+        }
+    }
+
+}
+
+//  dotnet new console 
+//  dotnet run
---
+++ testTwo/Program.cs
@@ -0,0 +1,73 @@
+﻿
+
+
+using System.Reflection;
+
+namespace ReflectionSpace
+{
+    public class ReflectionClass
+    {
+        public void ReflectionMethod()
+        {
+            Console.WriteLine("ReflectionMethod");
+        }
+
+
+        public static void Main()
+        {
+            // 获取类型
+            // Type type = typeof(ReflectionClass);
+            Assembly assembly = Assembly.LoadFrom(@"D:\localhostWebPage\C#\testOne\bin\Debug\net8.0\testOne");
+            Type[] types = assembly.GetTypes();
+            for (int i = 0; i < types.Length; i++)
+            {
+                Console.WriteLine("types ==" +types[i]);
+            }
+            // 加载 程序集 中的额一个 对象
+            Type testClassType = assembly.GetType("testNameSpace.testClass");
+
+            // h获取所有的成员
+            MemberInfo[] members = testClassType.GetMembers();
+            foreach (MemberInfo member in members)
+            {
+                Console.WriteLine("member == " + member);
+            }
+            // 通过 反射 实例化一个  对象
+            // 先得到 枚举的 type  再得到 可以传入 的参数
+
+            Type testClassEnumType = assembly.GetType("testNameSpace.testClass+Color");
+            Console.WriteLine("testClassEnumType == " + testClassEnumType);
+            // // 获取 枚举的 成员变量
+            FieldInfo[] fields = testClassEnumType.GetFields();
+            foreach (FieldInfo field in fields)
+            {
+                Console.WriteLine("field == " + field);
+            }
+
+            FieldInfo red = testClassEnumType.GetField("red");
+
+            // Console.WriteLine("red == " + red, "= red.GetValue(null) =", red.GetValue(null));
+            // 获取 枚举的 成员变量
+            // red.GetValue(null)  枚举 得到 具体的值
+            object testClassObj = Activator.CreateInstance(testClassType, 1, "string", red.GetValue(null));
+            // Console.WriteLine("这里 " );
+            Console.WriteLine("testClassObj == " + testClassObj);
+            // 得到 枚举的 成员变量
+            // red.GetValue(null)  枚举 得到 具体的值
+            // 得到 枚举的 成员变量
+            // red.GetValue(null)  枚举 得到 具体的值
+            // 得到 枚举的 成员变量
+            // red.GetValue(null)  枚举 得到 具体的值
+
+            // // 通过 反射  得到 对象的方法
+            MethodInfo method = testClassType.GetMethod("testMethod");
+            
+            Thread.Sleep(1000);
+            // // 执行方法
+            method.Invoke(testClassObj, null);
+    
+            
+            // 直接实例化
+        }
+    }
+}
